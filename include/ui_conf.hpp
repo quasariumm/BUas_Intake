9e1650fed077bc1a0f5ac66a48ae2cf848d03062
quasariumm <patrickvreeburg@protonmail.com>
2024-03-07 11:49:26 +0100

    Progress

--- include/ui_conf.hpp
+++ include/ui_conf.hpp
@@ -3,33 +3,52 @@
 
 // Handles the UI and the configuration.
 
+#include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/Drawable.hpp>
 #include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Font.hpp>
 #include <SFML/System/Vector2.hpp>
+#include <cstdint>
+#include <map>
 #include <string>
 #include <vector>
 
 namespace UIElements {
 
+  class Global {
+  public:
+    void initFont();
+    sf::Font& getFont() {return font;};
+  private:
+    sf::Font font;
+  };
+
   class Button {
   public:
     
-    Button(const sf::Texture& tOuter, const sf::Vector2u& vPos, const sf::Vector2u& vSize, const sf::Texture& tInner, const std::string buttonText = "") :
-    outer(tOuter), inner(tInner), text(buttonText), position(vPos), size(vSize) {};
+    Button(
+      const sf::Texture& tOuter, const sf::Vector2f& vPos, const sf::Vector2u& vSize, const sf::Texture& tInner,
+      const std::string buttonText = "", const unsigned short newCount = -1, const bool lockAspectRario = false
+    ) :
+    outer(tOuter), inner(tInner), text(buttonText), position(vPos), size(vSize), count(newCount), lockAspect(lockAspectRario) {};
 
     bool intersect(int x, int y);
     
-    virtual void draw();
+    void draw(sf::RenderWindow& window);
   
   private:
 
+    // Percentages/margins for both the inner sprite and the text.
     const float itemSize = 0.7f;
+    const float textSize = 0.6f;
 
     sf::Texture outer;
     sf::Texture inner;
+    bool lockAspect;
     std::string text;
+    int16_t count; // Diaplyed at the bottom right of the button if needed (-1 to turn off)
 
-    sf::Vector2u position;
+    sf::Vector2f position;
     sf::Vector2u size;
 
   };
@@ -37,16 +56,28 @@ namespace UIElements {
   class Inventory {
   public:
 
-    Inventory(const std::vector<std::string>& newItems, const sf::Texture& buttonOuter, const std::vector<sf::Texture>& textures);
+    Inventory(const std::vector<uint8_t>& newItems, const std::vector<int16_t>& newCounts, const sf::Texture& buttonOuter, const std::vector<sf::Texture>& textures);
     ~Inventory();
 
-    void setItems(std::vector<std::string>& newItems) {};
-    std::vector<std::string>& getItems() {return items;};
+    void setItems(std::vector<uint8_t>& newItems);
+    std::vector<uint8_t>& getItems() {return items;};
+
+    void setCounts(std::vector<int16_t>& newCounts) {counts = newCounts;};
+    std::vector<int16_t>& getCounts() {return counts;};
   
   private:
 
-    std::vector<std::string> items;
+    std::vector<uint8_t> items;
+    std::vector<int16_t> counts;
     std::vector<UIElements::Button> buttons;
+
+    std::string spritePath = std::string(RESOURCES_PATH) + "/res/sprites/";
+
+    std::map<uint8_t, std::filesystem::path> itemIdToPath = {
+      {0, std::filesystem::path(spritePath + "ball.png")},
+      {1, std::filesystem::path(spritePath + "ball.png")},
+      {2, std::filesystem::path(spritePath + "ball.png")},
+    };
 
   };
 
--- src/ui_conf.cpp
+++ src/ui_conf.cpp
@@ -1,21 +1,84 @@
 #include "../include/ui_conf.hpp"
-#include "SFML/System/Vector2.hpp"
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/System/Vector2.hpp>
 #include <SFML/Graphics/Sprite.hpp>
 #include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Text.hpp>
+#include <SFML/Graphics/Font.hpp>
+#include <cstdint>
+#include <filesystem>
+#include <stdexcept>
 #include <string>
 #include <vector>
+
+UIElements::Global global;
+
+void UIElements::Global::initFont() {
+  std::filesystem::path fontPath = RESOURCES_PATH;
+  fontPath += "font/Roboto.ttf";
+  if (!this->font.loadFromFile(fontPath)) {
+    throw std::runtime_error("Failed to load the font.");
+  }
+}
 
 bool UIElements::Button::intersect(int x, int y) {
   return (x >= this->position.x && x <= this->position.x + this->size.x && y >= this->position.y && y <= this->position.y + this->size.y);
 }
 
-void UIElements::Button::draw() {
-  return;
+void UIElements::Button::draw(sf::RenderWindow& window) {
+  sf::Sprite outerSprite(this->outer);
+  sf::Sprite innerSprite(this->inner);
+  sf::Text text(global.getFont(), this->text);
+
+  outerSprite.setScale(sf::Vector2f(this->size.x / static_cast<float>(this->outer.getSize().x), this->size.y / static_cast<float>(this->outer.getSize().y)));
+  outerSprite.setPosition(this->position);
+  window.draw(outerSprite);
+
+  if (this->inner.getSize().x != 0 && this->inner.getSize().y != 0) {
+    sf::Vector2f innerSize = this->itemSize * static_cast<sf::Vector2f>(this->size);
+    sf::Vector2f factors;
+    if (lockAspect) {
+      unsigned short smallestSide = (this->size.x < this->size.y) ? this->size.x : this->size.y;
+      smallestSide *= this->itemSize;
+      const float yToXAspect = this->inner.getSize().x / static_cast<float>(this->inner.getSize().y);
+      factors = sf::Vector2f((smallestSide * yToXAspect) / static_cast<float>(this->inner.getSize().x), smallestSide / static_cast<float>(this->inner.getSize().y));
+    } else {
+      factors = sf::Vector2f(innerSize.x / static_cast<float>(this->inner.getSize().x), innerSize.y / static_cast<float>(this->inner.getSize().y));
+    }
+    innerSprite.setScale(factors);
+    innerSprite.setPosition(this->position + 0.5f * (1.f - this->itemSize) * static_cast<sf::Vector2f>(this->size));
+    window.draw(innerSprite);
+  }
+
+  if (!this->text.empty()) {
+    text.setCharacterSize((static_cast<sf::Vector2f>(this->size) * this->textSize).x / this->text.length());
+    text.setPosition(this->position + 0.5f * (1.f - this->textSize) * static_cast<sf::Vector2f>(this->size));
+    window.draw(text);
+  }
+
+  if (this->count > -1) {
+    std::string countStr = std::to_string(this->count);
+    sf::Text countText(global.getFont(), countStr);
+    text.setCharacterSize(0.1 * this->size.x);
+    sf::Vector2f sizeF = static_cast<sf::Vector2f>(this->size);
+    sf::Vector2f bottomRightPadded = this->position + sizeF - 0.05f * sizeF;
+    text.setPosition(bottomRightPadded - sf::Vector2f(0.1f * sizeF.x * static_cast<float>(countStr.length()), 0));
+  }
 }
 
-UIElements::Inventory::Inventory(const std::vector<std::string>& newItems, const sf::Texture& buttonOuter, const std::vector<sf::Texture>& textures)
-: items(newItems) {
+// TODO: Refactor a bit. Turn textures into std::filesystem::path and in Button convert to sf::Texture.
+
+UIElements::Inventory::Inventory(const std::vector<uint8_t>& newItems, const std::vector<int16_t>& newCounts, const sf::Texture& buttonOuter, const std::vector<sf::Texture>& textures)
+: items(newItems), counts(newCounts) {
   for (sf::Texture texture : textures) {
-    this->buttons.push_back(UIElements::Button(buttonOuter, sf::Vector2u(), sf::Vector2u(), texture));
+    this->buttons.push_back(UIElements::Button(buttonOuter, sf::Vector2f(), sf::Vector2u(), texture));
+  }
+}
+
+void UIElements::Inventory::setItems(std::vector<uint8_t>& newItems) {
+  this->items = newItems;
+  this->buttons = {};
+  for (uint8_t item : newItems) {
+    // this->buttons.push_back();
   }
 }
